6a66e70530d7abded680f68a94a5cb3ae8940922
dg1418 <dg1418@naver.com>
2025-12-15 21:10:21 +0900

    도메인들 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ChatRoomModule } from './chat-room/chat-room.module';
+import { ChatMessageModule } from './chat-message/chat-message.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, ChatRoomModule, ChatMessageModule],
 })
 export class AppModule {}
